f701b1bd1eca19c34df85478b8cdb9ea9aa4c6c9
eaud <derek.s.mueller@gmail.com>
2016-02-18 11:36:45 -0500

    establishes initial routes

---
+++ tests/unit/routes/notebooks/notebook/sheets/sheet-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:notebooks/notebook/sheets/sheet', 'Unit | Route | notebooks/notebook/sheets/sheet', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
