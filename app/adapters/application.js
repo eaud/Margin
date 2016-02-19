1137eb45582ff5fbdbac5922dabf37111105436f
kaylee42 <kayleeknowles42@gmail.com>
2016-02-19 15:19:18 -0500

    sessions are working

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
   namespace: 'api/v1',
-  host: "http://localhost:3000"
+  host: "http://localhost:3000",
+  authorizer: "authorizer:devise"
 });
