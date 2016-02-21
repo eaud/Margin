3757b39fc08bfd48a5afc09a2656366dadac5e45
jappleba <josh.applebaum@flatironschool.com>
2016-02-21 17:24:33 -0500

    fix route links

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default ActiveModelAdapter.extend(DataAdapterMixin,{
   namespace: 'api/v1',
-  host: "http://margin-api.herokuapp.com",
+  host: "http://localhost:3000",
   authorizer: "authorizer:devise"
 });
--- app/routes/sheets.js
+++ app/routes/sheets.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin'
-export default Ember.Route.extend(ApplicationRouteMixin,{
+export default Ember.Route.extend(AuthenticatedRouteMixin,{
   model: function() {
         return this.store.findAll('sheet');
   }
