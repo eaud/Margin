bc4f6d90d5436525319d6980ffaf21aa0eccc622
kaylee42 <kayleeknowles42@gmail.com>
2016-02-23 19:20:17 -0500

    can add new sheet

--- app/models/sheet.js
+++ app/models/sheet.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   content: DS.attr('string'),
+  notebookid: DS.attr('number'),
   notebooks: DS.hasMany('notebook')
 });
--- app/routes/sheets/new.js
+++ app/routes/sheets/new.js
@@ -2,11 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return this.store.createRecord('sheet', {notebook: notebook})
+    return this.store.createRecord('sheet')
   },
   actions: {
     save(){
       let model = this.modelFor(this.routeName);
+      let notebook = this.modelFor('notebooks.notebook');
+      // model.get('notebooks').addObject(notebook);
+      model.set('notebookid', notebook.id);
+      debugger;
       model.save().then((savedSheet)=> {
         this.transitionTo("sheets.sheet", savedSheet);
       }).catch((reason)=>{
